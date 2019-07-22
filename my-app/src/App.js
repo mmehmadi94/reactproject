7e4730abb4f4a909b841058a7b7ffc39703bebbb
Manal Almehmadi <mmehmadi94@gmail.com>
2019-07-22 22:01:50 +0300

    react website

---
+++ my-app/src/App.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+
+  return (
+    <div className="App">
+
+      <body>
+           <div className="About us">
+
+                  <h3>Upload CV</h3>
+           <hr/>
+                  <h3>Preview the uploaded CV's</h3>
+           <hr/>
+
+           <h3>About us</h3>
+
+           <div class="our-container">
+            <div>
+              <p> Group leader </p>
+              <p> some information....</p>
+            </div>
+
+            <div>
+              <p> Group member 1 </p>
+              <p> some information....</p>
+            </div>
+
+            <div>
+              <p> Group member 2 </p>
+              <p> some information....</p>
+           </div>
+
+          </div>
+
+          </div>
+      </body>
+    </div>
+  );
+}
+
+export default App;
